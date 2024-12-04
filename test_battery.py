0b1f9a17e2e5fcc718c5d54332bf1aa9693c9e37
cacutler <116305038+cacutler@users.noreply.github.com>
2024-12-04 14:02:57 -0700

    Added tests for recharge function

--- test_battery.py
+++ test_battery.py
@@ -13,7 +13,28 @@ def partially_charged_battery():
 def describe_Battery():
     def describe_recharge():
         # your test cases here
-        pass
+        def it_recharges_with_a_positive_amount(partially_charged_battery):
+            assert partially_charged_battery.mCharge == 70
+            return_value = partially_charged_battery.recharge(20)
+            assert partially_charged_battery.mCharge == 90
+            assert return_value
+        def it_does_not_recharge_with_a_negative_amount(partially_charged_battery):
+            assert partially_charged_battery.mCharge == 70
+            return_value = partially_charged_battery.recharge(-5)
+            assert partially_charged_battery.mCharge != 65
+            assert partially_charged_battery.mCharge == 70
+            assert not return_value
+        def it_does_not_recharge_with_a_zero_amount(partially_charged_battery):
+            assert partially_charged_battery.mCharge == 70
+            return_value = partially_charged_battery.recharge(0)
+            assert partially_charged_battery.mCharge == 70
+            assert not return_value
+        def it_does_not_exceed_capacity_on_recharge(partially_charged_battery):
+            assert partially_charged_battery.mCharge == 70
+            return_value = partially_charged_battery.recharge(31)
+            assert partially_charged_battery.mCharge != 101
+            assert partially_charged_battery.mCharge == 100
+            assert return_value
     def describe_drain():
         # your test cases here
         pass
