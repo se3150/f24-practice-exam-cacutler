9ebc1950a80245b2711c8977520b2b5d81f03d55
cacutler <116305038+cacutler@users.noreply.github.com>
2024-12-04 13:42:33 -0700

    Updated file styling

--- test_battery.py
+++ test_battery.py
@@ -1,26 +1,19 @@
 import pytest
 from battery import Battery
 from unittest.mock import Mock
-
 todo = pytest.mark.skip(reason='todo: pending spec')
-
 @pytest.fixture
 def charged_battery():
     return Battery(100)
-
 @pytest.fixture
 def partially_charged_battery():
     b = Battery(100)
     b.mCharge = 70
     return b
-
-
 def describe_Battery():
-
     def describe_recharge():
         # your test cases here
-
+        pass
     def describe_drain():
         # your test cases here
-
-
+        pass
